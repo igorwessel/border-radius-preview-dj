55e31b263209862c4994b49f5c983777df1dde0b
igorwessel <igor_wessel@hotmail.com>
2020-02-20 09:27:35 -0300

    create app for django

---
+++ border_radius_preview/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for border_radius_preview project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'border_radius_preview.settings')
+
+application = get_asgi_application()
